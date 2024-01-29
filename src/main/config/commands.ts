c2e1ce730096ebf6f8c0ecec0806b99dcc8a3d86
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-01-29 18:57:00 -0300

    logs

--- src/main/config/commands.ts
+++ src/main/config/commands.ts
@@ -1,9 +1,12 @@
 import { ChannelType, Client } from "discord.js";
 import { Server, Socket } from "socket.io";
+import { logger } from "../../utils/logger";
 
 export const makeCommands = (client: Client<boolean>, server: Server) => {
   server.on("connection", async (socket: Socket) => {
+    logger.info("new connection");
     socket.on("chatLog", async (data: any) => {
+      logger.info("chatLog");
       const channel = client.channels.cache.get("1201614693341606009");
       if (channel && channel.type == ChannelType.GuildText) {
         channel.send(data);
