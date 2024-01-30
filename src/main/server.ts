c9862a944a0d19bce67652c0f764816c1982cc98
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-01-30 13:35:27 -0300

    fix: bot is not working on server

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,10 +3,9 @@ import { client, server } from "./config/app";
 import { env } from "./config/env";
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  server.listen(env.port, () => {
+    logger.info("listening on port " + env.port);
+  });
 });
 
 client.login(env.token);
-server.listen(env.port, () => {
-  logger.info("listening on port " + env.port);
-});
