98743bfb904acb82fc3ce2236a272bc77ad86d7f
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-01-30 13:37:47 -0300

    chore: update lgosa

--- src/main/config/commands.ts
+++ src/main/config/commands.ts
@@ -20,22 +20,22 @@ export const makeCommands = (client: Client<boolean>, server: Server) => {
   server.on("connection", async (socket: Socket) => {
     logger.info("new connection");
     socket.on("chatLog", async (data: any) => {
-      logger.info("chatLog");
+      logger.info("chatLog", data);
       sendMessage("1201614693341606009", data);
     });
 
     socket.on("reportLog", async (data: any) => {
-      logger.info("reportLog");
+      logger.info("reportLog", data);
       sendMessage("1201654662579621999", data);
     });
 
     socket.on("adminLog", async (data: any) => {
-      logger.info("adminLog");
+      logger.info("adminLog", data);
       sendMessage("1201657417226780702", data);
     });
 
     socket.on("banLog", async (data: any) => {
-      logger.info("banLog");
+      logger.info("banLog", data);
       sendMessage("1201658092836892803", data);
     });
   });
