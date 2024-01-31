3a5c3c5df2a52e9cd2b97302a63bb15a24ee6df0
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-01-30 22:46:09 -0300

    feat: set up kill and teamKill logs

--- src/main/config/commands.ts
+++ src/main/config/commands.ts
@@ -5,38 +5,51 @@ import { env } from "./env";
 import { clientSocket } from "./app";
 
 export const makeCommands = (client: Client<boolean>) => {
-  const sendMessage = (channelId: string, data: any) => {
-    try {
-      const channel = client.channels.cache.get(channelId);
-      if (channel && channel.type == ChannelType.GuildText && data != "") {
-        const dataFormatter = "```" + data + "```";
-        channel.send(dataFormatter);
-        return;
-      }
-
-      console.error("Canal inválido ou não suportado para mensagens diretas.");
-    } catch (err) {
-      logger.error(err);
-    }
-  };
-
   clientSocket.on("chat", async (data: any) => {
-    logger.info("chatLog", data);
+    logger.debug("Event executed: chatLog", data);
     sendMessage("1201614693341606009", data);
   });
 
   clientSocket.on("reportLog", async (data: any) => {
-    logger.info("reportLog", data);
+    logger.debug("Event executed: reportLog", data);
     sendMessage("1201654662579621999", data);
   });
 
   clientSocket.on("adminLog", async (data: any) => {
-    logger.info("adminLog", data);
+    logger.debug("Event executed: adminLog", data);
     sendMessage("1201657417226780702", data);
   });
 
   clientSocket.on("banLog", async (data: any) => {
-    logger.info("banLog", data);
+    logger.debug("Event executed: banLog", data);
     sendMessage("1201658092836892803", data);
   });
+
+  clientSocket.on("kill", async (data: any) => {
+    logger.info("Event executed: kill", data);
+    sendMessage("1202061441394544710", data);
+  });
+
+  clientSocket.on("teamKill", async (data: any) => {
+    logger.debug("Event executed: teamKill", data);
+    sendMessage("1202063119778861127", data);
+  });
+
+  const sendMessage = (channelId: string, data: any) => {
+    try {
+      const channel = client.channels.cache.get(channelId);
+
+      if (!channel || channel.type != ChannelType.GuildText || data == "") {
+        console.error(
+          "Canal inválido ou não suportado para mensagens diretas."
+        );
+        return;
+      }
+
+      const dataFormatter = "```" + data + "```";
+      channel.send(dataFormatter);
+    } catch (err) {
+      logger.error(err);
+    }
+  };
 };
